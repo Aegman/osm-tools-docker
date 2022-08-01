63f03e3ec8e9b76b7d34869ee7c67bf94e7113d7
Andy E <43781842+Aegman@users.noreply.github.com>
2022-08-01 17:04:04 +0100

    Update example-extract-highways.sh

--- example-extract-highways.sh
+++ example-extract-highways.sh
@@ -1,3 +1,9 @@
+# Copyright (c) 2022, Andy Egleton
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. 
+
 osmconvert peru-latest.osm.pbf -o=peru.o5m
 osmfilter peru.o5m --keep="highway=" -o=highways.osm
 ogr2ogr -skipfailures -f GPKG highways.gpkg highways.osm lines
